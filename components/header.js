91bc619ea4bb505a36019544fdc6b4338af80ea2
VictorMont03 <victormonteir2@gmail.com>
2022-10-06 18:12:27 -0300

    adding files api

--- components/header.js
+++ components/header.js
@@ -1,48 +1,42 @@
 export default function Header() {
   return (
-    <header className="tw-flex tw-items-center tw-z-[100] tw-bg-white tw-w-[100%] tw-px-[5%] tw-fixed tw-top-0 tw-left-0 tw-justify-between tw-py-[15px] tw-border-black tw-border-b-[1px]">
+    <header className="tw-flex tw-items-center tw-z-[400] tw-bg-white tw-w-[100%] tw-px-[5%] tw-fixed tw-top-0 tw-left-0 tw-justify-between tw-py-[15px] tw-border-black tw-border-b-[1px]">
       <div className="tw-flex">
         <img src="/assets/icons/logo.svg" className="tw-w-[100px]" />
       </div>
       <div>
         <a
           href="/"
-          target="_blank"
           className="tw-mx-[30px] hover:tw-text-blue tw-transition-[0.4s]"
         >
           Home
         </a>
         <a
           href="/#sobre"
-          target="_blank"
           className="tw-mx-[30px] hover:tw-text-blue tw-transition-[0.4s]"
         >
           Sobre
         </a>
         <a
           href="/produtos"
-          target="_blank"
           className="tw-mx-[30px] hover:tw-text-blue tw-transition-[0.4s]"
         >
           Produtos
         </a>
         <a
           href="/#catalogo"
-          target="_blank"
           className="tw-mx-[30px] hover:tw-text-blue tw-transition-[0.4s]"
         >
           Catálogo
         </a>
         <a
           href="/#videos"
-          target="_blank"
           className="tw-mx-[30px] hover:tw-text-blue tw-transition-[0.4s]"
         >
           Vídeos
         </a>
         <a
           href="#contato"
-          target="_blank"
           className="tw-ml-[30px] hover:tw-text-blue tw-transition-[0.4s]"
         >
           Contato
---
+++ components/navbar.js
@@ -0,0 +1,24 @@
+export default function Navbar({ categories }) {
+  console.log(categories);
+  return (
+    <nav className="tw-absolute tw-left-0 tw-z-[300] tw-w-[15%] tw-bg-blue tw-px-[10px] tw-py-[30px]">
+      <h1 className="tw-text-white ">
+        LINHA DE PRODUTOS
+        <hr className="tw-h-[1px] tw-w-[80%] tw-mb-[20px] tw-mt-[5px]" />
+      </h1>
+      {categories.categories.map((category, i) => (
+        <div className="tw-mb-[20px]">
+          <h3 className="tw-font-light tw-text-white">{category.name}</h3>
+          {category.products.map((product, i) => (
+            <a
+              href={`/produtos/${category.slug}?product=${product.slug}`}
+              className="tw-text-white tw-pl-[10px] tw-text-[14px] tw-font-light hover:tw-underline"
+            >
+              ● {product.name}
+            </a>
+          ))}
+        </div>
+      ))}
+    </nav>
+  );
+}
