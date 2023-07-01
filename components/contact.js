7dd0baf6ec29f8f1e011cb99c2dbf4da9db000fb
Pedro <pedro.neto72pn@gmail.com>
2023-06-30 21:50:14 -0300

    Adding contact feedback

--- components/contact.js
+++ components/contact.js
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import React, { useRef, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Input from "react-input-mask";
 import axios from "axios";
 
 export default function Contact() {
@@ -32,104 +31,100 @@ export default function Contact() {
 
     try {
       setLoading("Enviando...");
-      // console.log(mail);
 
       const res = await axios.post(
         "./api/mail",
-        {
-          body,
-        },
+        { body },
         {
           headers: {
             "Access-Control-Allow-Origin": "*",
-            // "Access-Control-Allow-Origin": "https://www.deviumlabs.com.br",
           },
         }
       );
       console.log(res.data);
 
       toast.success("Mensagem enviada!");
-
-      setLoading("ENVIAR");
-      // setValue("name", "");
-      // setValue("email", "");
-      // setValue("phone", "");
-      // setValue("message", "");
     } catch (e) {
       console.log(e);
-      setLoading("Clique aqui");
       toast.error("Houve um erro, por favor tente novamente mais tarde!");
+    } finally {
+      setLoading("ENVIAR");
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="tw-px-[20px] tw-mb-[100px] tw-pt-[200px] tw-mt-[-200px] tw-flex tw-flex-col tw-items-center"
-      id="contato"
-    >
-      <div className="tw-mb-[30px]">
-        <small className="tw-text-blue tw-font-light">CONTATO</small>
-        <h1 className="tw-text-[30px] tw-leading-[30px]">Entre em contato</h1>
-      </div>
-      <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
-        <label htmlFor="">Nome:</label>
-        <input
-          type="text"
-          name="name"
-          {...register("name", { required: true })}
-          className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
-        />
-        {errors.name && <span className="tw-text-red">*Campo obrigatório</span>}
-      </div>
-      <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
-        <label htmlFor="">Telefone:</label>
-        <input
-          type="tel"
-          name="phone"
-          mask={
-            !!phoneWatcher && phoneWatcher.length <= 14
-              ? "(99) 9999-9999?"
-              : "(99) 99999-9999"
-          }
-          formatChars={{ 9: "[0-9]", "?": "[0-9 ]" }}
-          maskChar={null}
-          {...register("phone", { required: true })}
-          className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
-        />
-        {errors.phone && (
-          <span className="tw-text-red">*Campo obrigatório</span>
-        )}
-      </div>
-      <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
-        <label htmlFor="">E-mail:</label>
-        <input
-          type="email"
-          name="email"
-          {...register("email", { required: true })}
-          className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
-        />
-        {errors.email && (
-          <span className="tw-text-red">*Campo obrigatório</span>
-        )}
-      </div>
-      <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
-        <label htmlFor="">Mensagem</label>
-        <textarea
-          name="message"
-          {...register("message", { required: true })}
-          className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
-        />
-        {errors.message && (
-          <span className="tw-text-red">*Campo obrigatório</span>
-        )}
-      </div>
-      <button
-        type="submit"
-        className="tw-bg-blue tw-text-white tw-w-[240px] tw-h-[50px] hover:tw-bg-white hover:tw-border-blue hover:tw-border-[1px] hover:tw-text-blue tw-transition-300"
+    <>
+      <ToastContainer />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="tw-px-[20px] tw-mb-[100px] tw-pt-[200px] tw-mt-[-200px] tw-flex tw-flex-col tw-items-center"
+        id="contato"
       >
-        {loading}
-      </button>
-    </form>
+        <div className="tw-mb-[30px]">
+          <small className="tw-text-blue tw-font-light">CONTATO</small>
+          <h1 className="tw-text-[30px] tw-leading-[30px]">Entre em contato</h1>
+        </div>
+        <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
+          <label htmlFor="">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            {...register("name", { required: true })}
+            className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
+          />
+          {errors.name && (
+            <span className="tw-text-red">*Campo obrigatório</span>
+          )}
+        </div>
+        <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
+          <label htmlFor="">Telefone:</label>
+          <input
+            type="tel"
+            name="phone"
+            mask={
+              !!phoneWatcher && phoneWatcher.length <= 14
+                ? "(99) 9999-9999?"
+                : "(99) 99999-9999"
+            }
+            formatChars={{ 9: "[0-9]", "?": "[0-9 ]" }}
+            maskChar={null}
+            {...register("phone", { required: true })}
+            className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
+          />
+          {errors.phone && (
+            <span className="tw-text-red">*Campo obrigatório</span>
+          )}
+        </div>
+        <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
+          <label htmlFor="">E-mail:</label>
+          <input
+            type="email"
+            name="email"
+            {...register("email", { required: true })}
+            className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
+          />
+          {errors.email && (
+            <span className="tw-text-red">*Campo obrigatório</span>
+          )}
+        </div>
+        <div className="tw-flex tw-flex-col tw-w-full tw-max-w-[600px] tw-mb-[20px]">
+          <label htmlFor="">Mensagem</label>
+          <textarea
+            name="message"
+            {...register("message", { required: true })}
+            className="tw-border-blue tw-border-[1px] tw-py-[12px] tw-px-[12px]"
+          />
+          {errors.message && (
+            <span className="tw-text-red">*Campo obrigatório</span>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="tw-bg-blue tw-text-white tw-w-[240px] tw-h-[50px] hover:tw-bg-white hover:tw-border-blue hover:tw-border-[1px] hover:tw-text-blue tw-transition-300"
+        >
+          {loading}
+        </button>
+      </form>
+    </>
   );
 }
