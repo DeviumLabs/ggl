1c807ee54c4d1cbcacacf6334968f90906e18bb2
Felipe Schandler <felipeschandle@gmail.com>
2025-08-05 15:57:33 -0300

    feat: melhoria no SEO

--- pages/produtos/[slug].js
+++ pages/produtos/[slug].js
@@ -65,7 +65,9 @@ export default function SingleProduct({ product, categories }) {
       "@type": "Offer",
       "url": `https://www.gglmoveis.com.br/produtos/${categories.categoryArray[0].slug}?product=${product.slug}`,
       "availability": "https://schema.org/InStock",
-      "itemCondition": "https://schema.org/NewCondition"
+      "itemCondition": "https://schema.org/NewCondition",
+      "priceCurrency": "BRL",
+      "price": "0.00"
     }
   };
 
