532f7cf8d30db05a7a7dc518b3fb1ca0bc15ba33
Felipe Schandler <felipeschandle@gmail.com>
2025-08-12 00:35:28 -0300

    feat: Adicionando tag do google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -20,23 +20,31 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16882485681"></script>
-          <script dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-16882485681');`
-          }} />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-16882485681"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-16882485681');
+                gtag('config', 'G-ESC22234MN');
+              `,
+            }}
+          />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
             href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700&family=Nanum+Gothic:wght@700&display=swap"
             rel="stylesheet"
           />
           <link rel="icon" href="/logo.svg" />
           <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff"></meta>
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
