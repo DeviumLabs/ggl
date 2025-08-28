31ce98eebd235a5d50871bf8931d0db9a1e5547a
Felipe <felipeschandle@gmail.com>
2025-08-28 11:36:37 -0300

    fix: retornando arquivo antigo para teste

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-// document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -6,23 +5,21 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-          <link rel="preconnect" href="https://www.googletagmanager.com" />
-          <link rel="preconnect" href="https://www.google-analytics.com" />
           <link rel="icon" href="/logo.svg" type="image/svg+xml" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
-          <Main />
-          <NextScript />
           <noscript
             dangerouslySetInnerHTML={{
               __html: `
                 <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMZW62HG"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
               `,
             }}
           />
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
